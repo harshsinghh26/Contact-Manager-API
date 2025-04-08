58bfd2794d81f6e2bc3e47e1f5e5e70b959c25e7
harshsinghh26 <harshsi343@gmail.com>
2025-04-08 17:32:47 +0530

    define user model and save password in hash format and create jwt tokens

---
+++ src/models/users.models.js
@@ -0,0 +1,84 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required!'],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+// Save the password in database in hash format
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// check if Password is correct or not
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// generate Access Web Token
+
+userSchema.methods.createAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: _id,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    },
+  );
+};
+
+// genrate Refresh Web Token
+
+userSchema.methods.createRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: _id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    },
+  );
+};
+
+export const User = mongoose.model('User', userSchema);
