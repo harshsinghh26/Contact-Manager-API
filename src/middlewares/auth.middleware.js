b6c21ecf66e93f3849fcb36eb96d8550290b3543
harshsinghh26 <harshsi343@gmail.com>
2025-04-09 22:21:23 +0530

    added logout feature

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,7 +6,7 @@ import { User } from '../models/users.models.js';
 const verifyJWT = ansycHandler(async (req, res, next) => {
   try {
     const token =
-      req.cookie?.accessToken ||
+      req.cookies?.accessToken ||
       req.header('Authorization')?.replace('Bearer ', '');
 
     if (!token) {
@@ -14,14 +14,16 @@ const verifyJWT = ansycHandler(async (req, res, next) => {
     }
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-    const user = await User.findById(decodedToken?._id);
+    const user = await User.findById(decodedToken?._id).select(
+      '-password -refreshToken',
+    );
     if (!user) {
       throw new ApiError(404, 'User not Exist!!');
     }
     req.user = user;
     next();
   } catch (error) {
-    console.log('Unauthorize access', error);
+    throw new ApiError(401, error?.message || 'Inavlid Access');
   }
 });
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,11 @@
 import { Router } from 'express';
-import { userLogin, userRegister } from '../controller/users.controller.js';
+import {
+  userLogin,
+  userLogout,
+  userRegister,
+} from '../controller/users.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
@@ -9,5 +14,6 @@ router
   .post(upload.fields([{ name: 'avatar', maxCount: 1 }]), userRegister);
 
 router.route('/login').post(userLogin);
+router.route('/logout').post(verifyJWT, userLogout);
 
 export default router;
