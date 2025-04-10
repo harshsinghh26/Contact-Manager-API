784d7f1c1cd29fc511101f4733f34528f4c2af95
harshsinghh26 <harshsi343@gmail.com>
2025-04-10 16:29:55 +0530

    added contact Schema

---
+++ src/models/contact.models.js
@@ -0,0 +1,30 @@
+import mongoose, { Schema } from 'mongoose';
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      toLowerCase: true,
+    },
+    address: {
+      type: String,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Contact = mongoose.model('Contact', contactSchema);
